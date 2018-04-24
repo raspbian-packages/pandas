d36d32d2f7d53d7838fa7a90d7385d2642deb626
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2018-04-24 20:09:20 +0100

    deb_fix_test_failure_test_basic_indexing
    Gbp-Pq: Name deb_fix_test_failure_test_basic_indexing

--- pandas/core/dtypes/common.py
+++ pandas/core/dtypes/common.py
@@ -1192,7 +1192,10 @@ def _is_unorderable_exception(e):
     """
 
     if PY36:
-        return "'>' not supported between instances of" in str(e)
+        # Temporary fix for Debian, reference:
+        # https://github.com/pandas-dev/pandas/pull/17724
+        return any("'{}' not supported between instances of".format(op) in
+                   str(e) for op in ('>', '<'))
 
     elif PY3:
         return 'unorderable' in str(e)
