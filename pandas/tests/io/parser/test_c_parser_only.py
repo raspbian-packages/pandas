329f69cf8c099416e5128989180b3ede6821e1f9
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2020-06-28 21:47:22 +0100

    Fix or skip tests that assume the wrong endianness
    This is a bug in the tests not pandas itself -
    the expected values explicitly specify little-endian
    
    Author: Rebecca N. Palmer <rebecca_palmer@zoho.com>
    Forwarded: no
    
    
    Gbp-Pq: Name tests_dont_assume_endian.patch

--- pandas/tests/io/parser/test_c_parser_only.py
+++ pandas/tests/io/parser/test_c_parser_only.py
@@ -129,7 +129,7 @@ nan 2
             "the dtype timedelta64 is not supported for parsing",
             dict(dtype={"A": "timedelta64", "B": "float64"}),
         ),
-        ("the dtype <U8 is not supported for parsing", dict(dtype={"A": "U8"})),
+        ("the dtype [<>]U8 is not supported for parsing", dict(dtype={"A": "U8"})),
     ],
     ids=["dt64-0", "dt64-1", "td64", "<U8"],
 )
