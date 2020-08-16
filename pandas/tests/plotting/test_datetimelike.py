e9a0e556508abd632f1b638cb15aa0e6426c67af
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2020-08-16 20:09:14 +0100

    Don't fail tests on harmless changes to dependencies
    Ignore deprecation warnings added in
    Python 3.8, matplotlib 3.2, jedi 0.16/0.17
    (one jedi deprecation appears to come from IPython because it uses stacklevel=2)
    
    Don't assert that matplotlib rejects shorthand hex colors,
    as from 3.2 it accepts them:
    https://matplotlib.org/users/prev_whats_new/whats_new_3.2.0.html#digit-and-4-digit-hex-colors
    
    Ignore change to date axis default range/ticks
    
    Author: Rebecca N. Palmer <rebecca_palmer@zoho.com>
    Forwarded: no
    
    
    Gbp-Pq: Name ignore_matplotlib_warning.patch

--- pandas/tests/plotting/test_datetimelike.py
+++ pandas/tests/plotting/test_datetimelike.py
@@ -1520,11 +1520,7 @@ class TestTSPlot(TestPlotBase):
         ax.scatter(x="time", y="y", data=df)
         self.plt.draw()
         label = ax.get_xticklabels()[0]
-        if self.mpl_ge_3_0_0:
-            expected = "2017-12-08"
-        else:
-            expected = "2017-12-12"
-        assert label.get_text() == expected
+        assert label.get_text() in ["2018-01-01","2017-12-08","2017-12-12"] # matplotlib version dependent
 
 
 def _check_plot_works(f, freq=None, series=None, *args, **kwargs):
