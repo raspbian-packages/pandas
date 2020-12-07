c66bb45f0e2b7b2a70abf17cfeda151c8c8bd31a
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2020-12-07 23:06:28 +0000

    Don't try to use git to find the version number
    Needed as Debian buildds use tarballs
    
    Author: Yaroslav Halchenko
    Forwarded: not-needed
    
    
    Gbp-Pq: Name deb_nonversioneer_version.patch

--- pandas/__init__.py
+++ pandas/__init__.py
@@ -177,13 +177,7 @@ from pandas.util._tester import test
 import pandas.testing
 import pandas.arrays
 
-# use the closest tagged version if possible
-from ._version import get_versions
-
-v = get_versions()
-__version__ = v.get("closest-tag", v["version"])
-__git_version__ = v.get("full-revisionid")
-del get_versions, v
+from .__version import version as __version__
 
 # GH 27101
 # TODO: remove Panel compat in 1.0
