9731fbaa1ed5c80086df88543e5620dc922933d5
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2021-01-12 21:06:04 +0000

    Remove code from Stack Overflow
    Stack Overflow content is CC-BY-SA licensed,
    which this package is not supposed to be.  These snippets may be
    too small to be copyrightable, but removing them to be safe.
    
    https://lists.debian.org/debian-legal/2020/04/threads.html#00018
    
    Author: Rebecca N. Palmer <rebecca_palmer@zoho.com>
    Forwarded: no - deletes some tests/examples without replacement
    
    
    Gbp-Pq: Name remove_ccbysa_snippets.patch

--- pandas/tests/groupby/test_categorical.py
+++ pandas/tests/groupby/test_categorical.py
@@ -828,29 +828,6 @@ def test_groupby_empty_with_category():
     tm.assert_series_equal(result, expected)
 
 
-def test_sort():
-
-    # https://stackoverflow.com/questions/23814368/sorting-pandas-
-    #        categorical-labels-after-groupby
-    # This should result in a properly sorted Series so that the plot
-    # has a sorted x axis
-    # self.cat.groupby(['value_group'])['value_group'].count().plot(kind='bar')
-
-    df = DataFrame({"value": np.random.randint(0, 10000, 100)})
-    labels = [f"{i} - {i+499}" for i in range(0, 10000, 500)]
-    cat_labels = Categorical(labels, labels)
-
-    df = df.sort_values(by=["value"], ascending=True)
-    df["value_group"] = pd.cut(
-        df.value, range(0, 10500, 500), right=False, labels=cat_labels
-    )
-
-    res = df.groupby(["value_group"], observed=False)["value_group"].count()
-    exp = res[sorted(res.index, key=lambda x: float(x.split()[0]))]
-    exp.index = CategoricalIndex(exp.index, name=exp.index.name)
-    tm.assert_series_equal(res, exp)
-
-
 def test_sort2():
     # dataframe groupby sort was being ignored # GH 8868
     df = DataFrame(
