58bea141f58275db752fc5feced06050cac47b4b
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2024-02-18 20:31:18 +0000

    Remove code from Stack Overflow
    Stack Overflow content is CC-BY-SA licensed,
    which this package is not supposed to be.  These snippets may be
    too small to be copyrightable, but removing them to be safe.
    
    https://lists.debian.org/debian-legal/2020/04/threads.html#00018
    
    Author: Rebecca N. Palmer <rebecca_palmer@zoho.com>
    Forwarded: no - deletes some tests/examples without replacement
    
    
    Gbp-Pq: Name remove_ccbysa_snippets.patch

--- pandas/tests/indexing/multiindex/test_chaining_and_caching.py
+++ pandas/tests/indexing/multiindex/test_chaining_and_caching.py
@@ -12,32 +12,6 @@ from pandas import (
 import pandas._testing as tm
 
 
-def test_detect_chained_assignment(using_copy_on_write):
-    # Inplace ops, originally from:
-    # https://stackoverflow.com/questions/20508968/series-fillna-in-a-multiindex-dataframe-does-not-fill-is-this-a-bug
-    a = [12, 23]
-    b = [123, None]
-    c = [1234, 2345]
-    d = [12345, 23456]
-    tuples = [("eyes", "left"), ("eyes", "right"), ("ears", "left"), ("ears", "right")]
-    events = {
-        ("eyes", "left"): a,
-        ("eyes", "right"): b,
-        ("ears", "left"): c,
-        ("ears", "right"): d,
-    }
-    multiind = MultiIndex.from_tuples(tuples, names=["part", "side"])
-    zed = DataFrame(events, index=["a", "b"], columns=multiind)
-
-    if using_copy_on_write:
-        with tm.raises_chained_assignment_error():
-            zed["eyes"]["right"].fillna(value=555, inplace=True)
-    else:
-        msg = "A value is trying to be set on a copy of a slice from a DataFrame"
-        with pytest.raises(SettingWithCopyError, match=msg):
-            zed["eyes"]["right"].fillna(value=555, inplace=True)
-
-
 @td.skip_array_manager_invalid_test  # with ArrayManager df.loc[0] is not a view
 def test_cache_updating(using_copy_on_write):
     # 5216
