31394e02e9994a3c96d10fe37ba73993ff28b6e1
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2024-04-21 13:50:13 +0100

    Fix test failures on 32-bit systems
    Author: Rebecca N. Palmer <rebecca_palmer@zoho.com>
    Bug-Debian: partly https://bugs.debian.org/1026351
    Forwarded: no
    
    
    Gbp-Pq: Name tests_dont_assume_64bit.patch

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -5,6 +5,7 @@ import re
 import numpy as np
 import pytest
 
+from pandas.compat import IS64
 from pandas.errors import (
     PerformanceWarning,
     SpecificationError,
@@ -2545,6 +2546,7 @@ def test_groupby_series_with_tuple_name():
     tm.assert_series_equal(result, expected)
 
 
+@pytest.mark.xfail(not IS64, reason="GH#38778: fail on 32-bit system", strict=False)
 @pytest.mark.parametrize(
     "func, values", [("sum", [97.0, 98.0]), ("mean", [24.25, 24.5])]
 )
@@ -2557,6 +2559,7 @@ def test_groupby_numerical_stability_sum_mean(func, values):
     tm.assert_frame_equal(result, expected)
 
 
+@pytest.mark.xfail(not IS64, reason="GH#38778: fail on 32-bit system", strict=False)
 def test_groupby_numerical_stability_cumsum():
     # GH#38934
     data = [1e16, 1e16, 97, 98, -5e15, -5e15, -5e15, -5e15]
