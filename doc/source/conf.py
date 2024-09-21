88891d3e818ce7c73ef38f7225895a4e78471ee1
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2024-09-21 17:21:40 +0100

    Allow building with Debian's pydata-sphinx-theme
    This is currently too old to have all the options upstream expects
    
    
    Gbp-Pq: Name sphinx_old_pydata_theme.patch

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -245,16 +245,7 @@ html_theme_options = {
     "footer_start": ["pandas_footer", "sphinx-version"],
     "github_url": "https://github.com/pandas-dev/pandas",
     "twitter_url": "https://twitter.com/pandas_dev",
-    "logo": {"image_dark": "https://pandas.pydata.org/static/img/pandas_white.svg"},
-    "navbar_align": "left",
-    "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links"],
-    "switcher": {
-        "json_url": "https://pandas.pydata.org/versions.json",
-        "version_match": switcher_version,
-    },
-    # This shows a warning for patch releases since the
-    # patch version doesn't compare as equal (e.g. 2.2.1 != 2.2.0 but it should be)
-    "show_version_warning_banner": False,
+    "navbar_end": ["navbar-icon-links"],
     "icon_links": [
         {
             "name": "Mastodon",
