3eb174fb4273fedef96f1b69fdb7dab5d1947552
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2024-10-11 10:18:43 +0100

    Fix test failures on 32-bit systems
    Author: Rebecca N. Palmer <rebecca_palmer@zoho.com>
    Bug-Debian: partly https://bugs.debian.org/1026351
    Forwarded: no
    
    
    Gbp-Pq: Name tests_dont_assume_64bit.patch

--- pandas/tests/series/methods/test_round.py
+++ pandas/tests/series/methods/test_round.py
@@ -30,8 +30,7 @@ class TestSeriesRound:
     def test_round_numpy_with_nan(self, any_float_dtype):
         # See GH#14197
         ser = Series([1.53, np.nan, 0.06], dtype=any_float_dtype)
-        with tm.assert_produces_warning(None):
-            result = ser.round()
+        result = ser.round() # on armhf, numpy warns
         expected = Series([2.0, np.nan, 0.0], dtype=any_float_dtype)
         tm.assert_series_equal(result, expected)
 
